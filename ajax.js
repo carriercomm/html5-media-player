42890fbd9e6e53855546079ce3977d5c5180273e
Sébastien Lucas <sebastien@slucas.fr>
2014-03-16 15:21:06 +0100

    Fix line ending / indentation style

--- ajax.js
+++ ajax.js
@@ -10,57 +10,57 @@ function isDefined(x) {
 }
 
 function getCurrentPath () {
-	return currentPath.join ("/");
+    return currentPath.join ("/");
 }
 
 function startDirectoryContent () {
-	$.ajax ({
-		'url': 'getpath.php',
-		'type': 'post',
-		'data': { 'path':  getCurrentPath () },
-		'dataType' : 'json',
-		'success': updateDirectoryContent});
+    $.ajax ({
+        'url': 'getpath.php',
+        'type': 'post',
+        'data': { 'path':  getCurrentPath () },
+        'dataType' : 'json',
+        'success': updateDirectoryContent});
 }
 
 function updateDirectoryContent (data) {
-	$("#explorer").empty ();
-	$.each(data, function () {
-		var dataAttribute = "";
-		if (this.isDir) {
-			dataAttribute = " data-directory='true'";
-		}
-		$("#explorer").append ('<li' + dataAttribute + '>' + this.path + '</li>')
-	});
+    $("#explorer").empty ();
+    $.each(data, function () {
+        var dataAttribute = "";
+        if (this.isDir) {
+            dataAttribute = " data-directory='true'";
+        }
+        $("#explorer").append ('<li' + dataAttribute + '>' + this.path + '</li>')
+    });
 }
 
 function handleDirectoryClickEvent (event) {
-	if (!isDefined ($(this).attr ("data-directory"))) {
-		return;
-	}
-	currentPath.push ($(this).text ());
-	startDirectoryContent ();
+    if (!isDefined ($(this).attr ("data-directory"))) {
+        return;
+    }
+    currentPath.push ($(this).text ());
+    startDirectoryContent ();
 }
 
 function topDirectory (event) {
-	if (event) {
-		event.preventDefault();
-	}
-	currentPath = new Array ();
-	currentPath.push (".");
-	startDirectoryContent ();
+    if (event) {
+        event.preventDefault();
+    }
+    currentPath = new Array ();
+    currentPath.push (".");
+    startDirectoryContent ();
 }
 
 function upDirectory (event) {
-	if (event) {
-		event.preventDefault();
-	}
-	if (currentPath.length == 1) return;
-	currentPath.pop ();
-	startDirectoryContent ();
+    if (event) {
+        event.preventDefault();
+    }
+    if (currentPath.length == 1) return;
+    currentPath.pop ();
+    startDirectoryContent ();
 }
 
 $(document).ready(function() {
-	topDirectory ();
+    topDirectory ();
     $("body").on ("click", "#explorer li", handleDirectoryClickEvent);
     $("#buttonHome").on ("click", topDirectory);
     $("#buttonUp").on ("click", upDirectory);
