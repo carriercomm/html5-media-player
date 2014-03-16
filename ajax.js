21970cf47888f4ee92a31fa8ce4bd3d7323816d9
Sébastien Lucas <sebastien@slucas.fr>
2014-03-16 15:18:28 +0100

    Handle going to root dir and upper directory

--- ajax.js
+++ ajax.js
@@ -2,13 +2,22 @@ $.ajaxSetup({
     cache: false
 });
 
-var currentPath = ".";
+var currentPath = new Array ();
 
-function startDirectoryContent (path) {
+function isDefined(x) {
+    var undefinedVar;
+    return x !== undefinedVar;
+}
+
+function getCurrentPath () {
+	return currentPath.join ("/");
+}
+
+function startDirectoryContent () {
 	$.ajax ({
 		'url': 'getpath.php',
 		'type': 'post',
-		'data': { 'path':  path },
+		'data': { 'path':  getCurrentPath () },
 		'dataType' : 'json',
 		'success': updateDirectoryContent});
 }
@@ -16,17 +25,43 @@ function startDirectoryContent (path) {
 function updateDirectoryContent (data) {
 	$("#explorer").empty ();
 	$.each(data, function () {
-		$("#explorer").append ('<li>' + this.path + '</li>')
+		var dataAttribute = "";
+		if (this.isDir) {
+			dataAttribute = " data-directory='true'";
+		}
+		$("#explorer").append ('<li' + dataAttribute + '>' + this.path + '</li>')
 	});
 }
 
 function handleDirectoryClickEvent (event) {
-	var path = $(this).text ();
-	currentPath += '/' + path;
-	startDirectoryContent (currentPath);
+	if (!isDefined ($(this).attr ("data-directory"))) {
+		return;
+	}
+	currentPath.push ($(this).text ());
+	startDirectoryContent ();
+}
+
+function topDirectory (event) {
+	if (event) {
+		event.preventDefault();
+	}
+	currentPath = new Array ();
+	currentPath.push (".");
+	startDirectoryContent ();
+}
+
+function upDirectory (event) {
+	if (event) {
+		event.preventDefault();
+	}
+	if (currentPath.length == 1) return;
+	currentPath.pop ();
+	startDirectoryContent ();
 }
 
 $(document).ready(function() {
-    startDirectoryContent (".");
+	topDirectory ();
     $("body").on ("click", "#explorer li", handleDirectoryClickEvent);
+    $("#buttonHome").on ("click", topDirectory);
+    $("#buttonUp").on ("click", upDirectory);
 });
